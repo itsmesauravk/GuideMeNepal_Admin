7d36ca518453ee0508bca025771640ed2449c69d
Saurav Karki <sauravkarki10.12@gmail.com>
2025-05-10 23:28:18 +0545

    creaetd: new pages

---
+++ src/app/guide-reports/page.tsx
@@ -0,0 +1,12 @@
+import GuideReports from "@/components/common/GuideReports"
+import React from "react"
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <GuideReports />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/location/page.tsx
@@ -0,0 +1,14 @@
+// import AdminLocationTrack from "@/components/common/AdminLocationTrack"
+
+import React from "react"
+
+const page = () => {
+  return (
+    <div className="w-full">
+      <h1>Hello </h1>
+      {/* <AdminLocationTrack /> */}
+    </div>
+  )
+}
+
+export default page
